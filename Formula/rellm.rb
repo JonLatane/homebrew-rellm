224ba17da4ab52aabb69553e991a47a07fea7f8e
Rellm CI <ci@jonline.io>
2026-09-20 19:16:21 +0000

    rellm 0.5.553-20260920190752-81f3045

--- Formula/rellm.rb
+++ Formula/rellm.rb
@@ -5,9 +5,9 @@
 class Rellm < Formula
   desc "Rellm federated social server"
   homepage "https://github.com/jonlatane/rellm"
-  url "https://github.com/jonlatane/rellm/releases/download/v0.5.553-ef76800/rellm-0.5.553-ef76800-macos-arm64.tar.gz"
-  sha256 "a250d498fd47a0f547763c89a000f380a10ac2f96c764a7410b95e5d19d41fcb"
-  version "0.5.553-ef76800"
+  url "https://github.com/jonlatane/rellm/releases/download/v0.5.553-20260920190752-81f3045/rellm-0.5.553-20260920190752-81f3045-macos-arm64.tar.gz"
+  sha256 "07afccc88632d141fce91aec2a748a6b72ab00231874aa1d7114c64efb689587"
+  version "0.5.553-20260920190752-81f3045"
   license "AGPL-3.0-only"
 
   depends_on arch: :arm64
